be9cf52d80e55b37a0f4196ed83482fbba3f6b59
cbb9556 <cbb9556@gmail.com>
2020-12-18 11:34:30 +0800

    new 543

--- 092.reverseListBetween/Solution.h
+++ 092.reverseListBetween/Solution.h
@@ -18,23 +18,34 @@ struct ListNode {
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int m, int n) {
+
+        //边界条件 反转的范围 为0 即 m == n； 空链表 ； 只有一个节点的链表
         if(head == nullptr || head->next == nullptr || m == n) return head;
+
+        //最后返回时 需要用到的 哨兵
         ListNode *dumHead = new ListNode(0);
         dumHead->next = head;
         
         ListNode *front = dumHead, *p, *q, *r;
         int i = 0;
+        
+        //front指到 m-1 那个元素， 
         for(; i < m-1; i++) front = front->next;    // front -> 第 m-1 个元素
-        p = front->next;
-        q = p->next;
+        p = front->next; // m
+        q = p->next; // m+1
+        
+        //三个指针 
         for(i += 2; i < n + 1; i++){    // i 最初跟踪 front；反转链表时，i 跟踪 p，故初始化为 i+2
-            r = q->next;
+            r = q->next; //
             q->next = p;
             p = q;
             q = r;
         }
+
         front->next->next = q;
         front->next = p;
+        
+        //防止m = 1时，front = dumHead 头节点 被放到尾端 结果 仅返回 head
         if(front == dumHead) head = front->next;
         delete dumHead;
         return head;
---
+++ 543.dimeterOfBinaryTree/main.cc
@@ -0,0 +1,60 @@
+#include "solution.h"
+#include <iostream>
+#include <queue>
+#include <string>
+#include <vector>
+using namespace std;
+using vecIter = std::vector<int>::iterator;
+
+TreeNode *createBinaryTree(vecIter beg, vecIter end)
+{
+    std::vector<TreeNode *> vec;
+    for (vecIter it = beg; it != end; ++it)
+        vec.push_back(new TreeNode(*it));
+    
+    for (int i = 0, pos = 0; pos != vec.size()-1; ++i)
+    {
+        vec[i]->left = vec[++pos];
+        vec[i]->right = vec[++pos];
+    }
+    return *vec.begin();
+}
+
+void print_bfs(TreeNode* p)
+{
+    if (!p) return;
+    std::vector<std::string> v;
+    std::queue<TreeNode *> q;
+    q.push(p);
+    do {
+        TreeNode *node = q.front(); q.pop();
+        if (node) 
+            v.push_back(std::to_string(node->val));
+        else {
+            v.push_back("#");
+            continue;
+        }
+        q.push(node->left); 
+        q.push(node->right);
+    } while (!q.empty());
+
+    auto found = std::find_if(v.rbegin(), v.rend(), [](const std::string &s){ return s != "#"; });
+    v.erase(found.base(), v.end());
+    for (const auto &s : v)
+        std::cout << s << ",";
+    std::cout << "\b " << std::endl; 
+}
+
+int main()
+{
+    std::vector<int> vec{1,2,3,4,5};
+    TreeNode* p = createBinaryTree(vec.begin(), vec.end()); 
+    Solution s;
+    print_bfs(p);
+    std::cout << s.diameterOfBinaryTree(p) << std::endl;
+    return 0;
+}
+
+
+
+
